7b529a512379969978ee79f73daec48be83c21d6
lvinyard <lvinyard98@gmail.com>
2018-10-12 18:09:24 -0700

    Yeee

--- Pokemon/Assets/Scripts/Player/PlayerMovement.cs
+++ Pokemon/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
+    public float moveSpeed;
+
+    private Animator anim;
+
+    /*
     Direction currentDir;
     Vector2 input;
     bool isMoving = false;
@@ -13,16 +18,37 @@ public class PlayerMovement : MonoBehaviour
     float t;
 
     public Sprite northSprite;
+
     public Sprite eastSprite;
+
     public Sprite southSprite;
+
     public Sprite westSprite;
 
     public float walkSpeed = 3f;
+    */
+
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+        }
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+        }
 
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        /*
         if (!isMoving)
         {
             input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
@@ -72,9 +98,12 @@ public class PlayerMovement : MonoBehaviour
                 StartCoroutine(Move(transform));
             }
         }
+        */
 
     }
+}
 
+    /*
     public IEnumerator Move(Transform entity)
     {
         isMoving = true;
@@ -102,3 +131,5 @@ enum Direction
     South,
     West
 }
+
+*/
